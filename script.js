26d7d307103906b2e938fafc281ad2d5042c6cf5
Nicole Franklin <Ncfranklin14@gmail.com>
2022-04-03 19:02:06 -0700

    psuedo code

---
+++ script.js
@@ -0,0 +1,13 @@
+// Show start quiz button
+// Click start button to being game
+    // Start timer 
+// Display 1st question with 4 answer options
+    // If timer runs out, game is over
+// User clicks answer
+    // If answer is correct, display "Correct" and move to question 2
+    // If answer is wrong, display "Wrong" and move to question 3
+// Run loop for all 5 questions
+// After question 5, show "Game Over" screen and form to add name to high score list with "submit" button
+// After user hits submit, show page with "see high scores" button and "Restart game" button
+    // If user selects "see high scores", go to high scores list from storage log
+    // If user clicks "Restart game" button, return to home page
